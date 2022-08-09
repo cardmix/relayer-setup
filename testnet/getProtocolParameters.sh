e630ea23e34a0980a6678d5790e8d6be6e9c124b
cardmix <91214090+cardmix@users.noreply.github.com>
2022-08-09 19:24:12 +0200

    Update scripts

--- testnet/getProtocolParameters.sh
+++ testnet/getProtocolParameters.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-CARDANO_NODE_SOCKET_PATH=node.sock
+export CARDANO_NODE_SOCKET_PATH=node.sock
 
 cardano-cli query protocol-parameters --testnet-magic 1097911063 --out-file protocol-parameters.json
