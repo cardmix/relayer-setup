cb4a06ccc977b456772969af8c24826bf8c83186
Cauterization <71918928+Cauterization@users.noreply.github.com>
2023-10-10 21:44:17 +0300

    Rename node_port to node_metrics_port

--- testnet-preprod/scripts/healthcheck.sh
+++ testnet-preprod/scripts/healthcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-node_port=12798
+node_metrics_port=12798
 kupo_port=1442
 relay_port=$(jq -r '.port' ../apps/encoins/config.json)
 relay_host=$(jq -r '.host' ../apps/encoins/config.json)
@@ -15,7 +15,7 @@ check_service()
     fi
 }
 
-node_check=$(curl "localhost:${node_port}/metrics") || node_check=$connErr
+node_check=$(curl "localhost:${node_metrics_port}/metrics") || node_check=$connErr
 kupo_check=$(curl "localhost:${kupo_port}/health") || kupo_check=$connErr
 wallet_check=$(cardano-wallet network information) || wallet_check=$connErr
 relay_check=$(curl "${relay_host}:${relay_port}/ping") || relay_check=$connErr
